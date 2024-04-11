1a639bbcda1f8a63fa8dc14c6fa2f5e0c8f97632
Kwasi Owusu-Donkor <kwasi.donkor@hubtel.com>
2024-04-11 23:07:43 +0000

    develop billboard on homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,16 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("0669fa29-485f-44da-aa66-4a1efda84071");
+
   return (  
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
  
